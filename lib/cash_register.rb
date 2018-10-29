67b40a1828e2b8be70086503dd4dffd86efbca61
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-29 02:07:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, price, quantity = 1)
+  def add_item(title, price, quantity = nil)
     self.total += price * quantity
 
     @items += [title] * quantity
