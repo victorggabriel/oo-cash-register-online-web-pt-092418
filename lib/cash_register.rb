c2b31480085cbcdc91746d0aa017fac20bba0049
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-29 02:02:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
 
-    self.items += [title] * quantity
+    @items += [title] * quantity
 
   end
 
